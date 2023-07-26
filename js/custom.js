766c84e994d324cc1e40f645cbcabbc687c85df9
hoangbaouyen <140375929+hoangbaouyen@users.noreply.github.com>
2023-07-26 14:06:17 +0700

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,26 @@
+// JavaScript Document
+var typed = new Typed('.element', {
+               	strings: ["Web Designer.", "Code Developer.","Apps Designer."],
+       	 	typeSpeed:100,
+        		backSpeed:50,
+        		loop:true,
+        		});
+$('.counter').countUp();
+
+
+function openCity(evt, cityName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
---
+++ js/tab.js
@@ -0,0 +1,14 @@
+// JavaScript Document
+function openCity2(evt, cityName) {
+var i, tabcontent, tablinks;
+tabcontent = document.getElementsByClassName("tabcontent2");
+for (i = 0; i < tabcontent.length; i++) {
+tabcontent[i].style.display = "none";
+}
+tablinks = document.getElementsByClassName("tablinks");
+for (i = 0; i < tablinks.length; i++) {
+tablinks[i].className = tablinks[i].className.replace(" active", "");}
+document.getElementById(cityName).style.display = "block";
+evt.currentTarget.className += " active";
+}
+document.getElementById("defaultOpen2").click();
